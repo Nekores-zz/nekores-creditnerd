3b5493e500b6dd0c5166bfa19c98bb994061095f
Nekores <sajidxullah@gmail.com>
2021-02-14 01:28:47 +0500

    Work update on Design Implementation

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ $("#myBtn").click(function () {
   var err = $(".error-msg");
   const inpVal = inp.val();
   console.log(inpVal.length);
-  if (inpVal.length > 12 && inpVal.length < 19) {
+  if (inpVal.length > 12 && inpVal.length <= 19) {
     console.log("valid");
   } else {
     inp.addClass("error");
@@ -22,4 +22,19 @@ $(document).ready(function () {
   $(".popover-btn").hover(function () {
     $(".tooltip-custom").toggleClass("d-none");
   });
+});
+
+document.getElementById("cardNumber").addEventListener("input", function (e) {
+  e.target.value = e.target.value
+    .replace(/[^\dA-Z]/g, "")
+    .replace(/(.{4})/g, "$1 ")
+    .trim();
+});
+$(".one").hide();
+$("input:radio").change(function () {
+  if ($(this).val() == "option1") {
+    $(".one").hide();
+  } else {
+    $(".one").show();
+  }
 });
